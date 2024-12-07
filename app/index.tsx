0f8660355445eafa4c4fe4137a6ecae5efedd386
Mihai Lazari <l3m3a3@gmail.com>
2024-12-07 14:26:27 +0200

    Add repro

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { useCallback, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { OptimizelyProvider, createInstance  } from "@optimizely/react-sdk";
+
+const optimizelyReactSdkVersion = require("@optimizely/react-sdk/package.json").version;
+
+const optimizely = createInstance({
+  sdkKey: process.env.EXPO_PUBLIC_OPTIMIZELY_SDK_KEY,
+  odpOptions: {
+    disabled: true,
+  },
+  // These are the defaults but set here to prevent SDK warnings
+  eventBatchSize: 10,
+  eventFlushInterval: 1000,
+});
+
+type UserInfo = Parameters<typeof optimizely.setUser>[0];
+
+const myUserInfo: UserInfo = { id: '1', attributes: {} };
 
 export default function Index() {
+  const [isReady, setIsReady] = useState(false);
+  const [user, setUser] = useState<UserInfo | undefined>(undefined);
+
+  const checkIsReady = useCallback(() => {
+    setIsReady(optimizely.isReady());
+  }, []);
+
+  const setUserInfo = useCallback(() => {
+    setUser(myUserInfo);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <OptimizelyProvider optimizely={optimizely} user={user}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ marginBottom: 30, fontSize: 15 }}>@optimizely/react-sdk {optimizelyReactSdkVersion}</Text>
+        <Text>Optimizely Ready: {JSON.stringify(isReady)}</Text>
+        <Button title="Re-check" onPress={checkIsReady} />
+        <Text>User info: {JSON.stringify(user)}</Text>
+        <Button title="Set user info" onPress={setUserInfo} />
+      </View>
+    </OptimizelyProvider>
   );
 }
